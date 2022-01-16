2ed60026b9329da6546b450a8fb364f59a6fc4f3
Martin Ruderer <martin.ruderer@gmail.com>
2022-01-16 10:13:22 +0100

    2020 day 19 - part 2 - now with ReadP
    Could not figure out why megaparsec would not consider all options..
    ReadP promises to do so and.. apparently.. does.

--- 2020/19/solution_2.hs
+++ 2020/19/solution_2.hs
@@ -1,7 +1,7 @@
 import qualified Control.Applicative as A
 import Control.Monad.Combinators.Expr (Operator (InfixL), makeExprParser)
 import Data.Either (fromRight)
-import Data.Map as M hiding (filter, map)
+import Data.Map as M hiding (null, filter, map)
 import Data.Maybe (isJust)
 import Data.Void (Void)
 import Maybes (fromJust)
@@ -9,24 +9,21 @@ import Text.Megaparsec
 import Text.Megaparsec.Char
 import qualified Text.Megaparsec.Char.Lexer as L
 import Prelude hiding (either)
+import Text.Megaparsec.Debug (dbg)
+import qualified Text.ParserCombinators.ReadP as R
 
 main :: IO ()
 main = do
-  content <- readFile "input.txt"
-
+  content <- readFile "input_2.txt"
 
   let input = fromJust $ parseMaybe parseInput content
   let rulesChecker = toChecker (fst input)
   let samples = snd input
 
-  let valids = filter (\x -> isJust (parseMaybe rulesChecker x)) samples
-  print valids
+  let valids = filter (checkChecker rulesChecker) samples
   print $ length valids
 
-  print ""
-
 type Parser = Parsec Void String
-
 type Rules = M.Map Int Rule
 
 data Rule
@@ -35,13 +32,20 @@ data Rule
   | RFollowed [Int]
   deriving (Show)
 
-toChecker :: Rules -> Parser String
-toChecker rules = toChecker' rules (fromJust $ M.lookup 0 rules)
+checkChecker :: R.ReadP Char -> String -> Bool
+checkChecker checker str = let
+  matches =  R.readP_to_S checker str
+  filtered = filter (\(a,xs) -> null xs) matches
+  in
+    not (null matches) && not (null filtered)
 
-toChecker' :: Rules -> Rule -> Parser String
-toChecker' rules (RLiteral c) = symbol [c]
-toChecker' rules (REither l r) = choice [try (toChecker' rules l), try (toChecker' rules r)]
-toChecker' rules (RFollowed rs) = head <$> mapM (toChecker' rules) [fromJust (M.lookup r rules) | r <- rs]
+toChecker :: Rules -> R.ReadP Char
+toChecker rules = toChecker' rules (rules ! 0)
+
+toChecker' :: Rules -> Rule -> R.ReadP Char
+toChecker' rules (RLiteral c) =  R.char c
+toChecker' rules (REither l r) =   toChecker' rules l R.+++  toChecker' rules r
+toChecker' rules (RFollowed rs) =  foldr1 (*>) (map (\r -> toChecker' rules (rules ! r) ) rs)
 
 sc = L.space (skipSome (char ' ')) A.empty A.empty
 
