ce270f6428fd1ade6b542a2dc6fd048f2389a250
Martin Ruderer <martin.ruderer@gmail.com>
2022-02-05 22:30:43 +0100

    2019: day 12 - part 2

--- 2019/12/solution.hs
+++ 2019/12/solution.hs
@@ -13,6 +13,8 @@ type Velocity = (Int, Int, Int)
 main = do
   print "Solution 1"
   print solution1
+  print "Solution 2"
+  print $ sol2 puzzleInput
 
 solution1 = energyAfter puzzleInput 1000
 
@@ -69,7 +71,55 @@ getTotalGravity p@(Planet pos velo) ps =
    in Planet (pos ^+^ newVelocity) newVelocity
 
 getNewVelocity :: Planet -> Planet -> Velocity
-getNewVelocity (Planet p@(x, y, z) v) (Planet (x', y', z') _) = (d x x', d y y', d z z')
+getNewVelocity (Planet (x, y, z) _) (Planet (x', y', z') _) = (d x x', d y y', d z z')
+  where
+    d :: Int -> Int -> Int
+    d u v
+      | u > v = -1
+      | u < v = 1
+      | otherwise = 0
+
+
+
+-- Oh well.. the "stupid" approach is too slow. But we can model the coordinates independently of each other..
+-- So we determine all "cycle counts" individually and take the lcm of those 3.
+type SliceVelo = Int
+
+type Slice = (Int, Int) -- pos and velo
+
+type Slices = [(Int, Int)]
+
+repeats :: Int -> [Slices] -> HashSet.HashSet Slices -> Int
+repeats n [] _ = undefined
+repeats n (s : ss) seen
+  | s `HashSet.member` seen = n
+  | otherwise = repeats (n + 1) ss (s `HashSet.insert` seen)
+
+iteratedSlice :: ((Int, Int, Int) -> Int) -> [Planet] -> [Slices]
+iteratedSlice pr input = iterate doSliceStep (toSlice pr input)
+
+sol2 :: [Planet] -> Int
+sol2 input =
+  let xs = iteratedSlice (\(a, b, c) -> a) input
+      ys = iteratedSlice (\(a, b, c) -> b) input
+      zs = iteratedSlice (\(a, b, c) -> c) input
+      rep x = repeats 0 x HashSet.empty
+      (x, y, z) = (rep xs, rep ys, rep zs)
+   in lcm x (lcm y z)
+
+toSlice :: ((Int, Int, Int) -> Int) -> [Planet] -> [Slice]
+toSlice pos ps = map (\(Planet p v) -> (pos p, pos v)) ps
+
+doSliceStep :: Slices -> Slices
+doSliceStep ps = map (\p -> getTotalSliceGravity p ps) ps
+
+getTotalSliceGravity :: Slice -> Slices -> Slice
+getTotalSliceGravity s ss =
+  let newVelo = snd s + sum (map (getNewSliceVelocity s) ss)
+   in (fst s + newVelo, newVelo)
+
+getNewSliceVelocity :: Slice -> Slice -> SliceVelo
+getNewSliceVelocity s t = d (fst s) (fst t)
   where
     d :: Int -> Int -> Int
     d u v
